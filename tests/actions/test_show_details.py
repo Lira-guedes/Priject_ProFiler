6a718bbe280d64d6eda982c7fc7db4c72152b756
Lira Guedes <liracrg@gmail.com>
2024-01-22 16:43:12 -0300

    fix: Requirement 3 bug

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,1 +1,14 @@
+import pytest
 from pro_filer.actions.main_actions import show_details  # NOQA
+
+
+@pytest.mark.parametrize(
+    "context, expected",
+    [
+      
+    ],
+)
+def test_show_details(context, expected, capsys):
+    show_details(context)
+    captured_output, _ = capsys.readouterr()
+    assert captured_output.strip() == expected
--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -6,25 +6,25 @@ from pro_filer.actions.main_actions import show_preview
     "context, expected",
     [
         ({'all_files': [], 'all_dirs': []},
-         "Found 0 files and 0 directories"),
+         "Found 0 files and 0 directories\n"),
         ({'all_files':
           ['a.txt', 'b.txt', 'c.txt', 'd.txt', 'e.txt', 'f.txt'],
           'all_dirs': []},
          "Found 6 files and 0 directories\n"
          "First 5 files: ['a.txt', 'b.txt', 'c.txt', 'd.txt', 'e.txt']\n"
-         "First 5 directories: []"),
+         "First 5 directories: []\n"),
         ({'all_files': [], 'all_dirs': ['ddd', 'eee', 'fff']},
          "Found 0 files and 3 directories\n"
          "First 5 files: []\n"
-         "First 5 directories: ['ddd', 'eee', 'fff']"),
+         "First 5 directories: ['ddd', 'eee', 'fff']\n"),
         ({'all_files': ['a.txt', 'b.txt', 'c.txt'],
           'all_dirs': ['ddd', 'eee', 'fff']},
          "Found 3 files and 3 directories\n"
          "First 5 files: ['a.txt', 'b.txt', 'c.txt']\n"
-         "First 5 directories: ['ddd', 'eee', 'fff']"),
+         "First 5 directories: ['ddd', 'eee', 'fff']\n"),
     ],
 )
 def test_show_preview(context, expected, capsys):
     show_preview(context)
-    captured_output, _ = capsys.readouterr()
-    assert captured_output.strip() == expected
+    result = capsys.readouterr()
+    assert result.out == expected
